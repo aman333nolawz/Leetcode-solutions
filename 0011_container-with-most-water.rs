9af772f0fc1716bed169f601f022879ae307ff85
Nolawz <67265008+aman333nolawz@users.noreply.github.com>
2024-07-13 18:14:57 +0530

    Added solution for 'container with most water'

---
+++ 0011_container-with-most-water.rs
@@ -0,0 +1,19 @@
+use std::cmp;
+
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let mut left = 0;
+        let mut right = height.len() - 1;
+        let mut max_area = 0;
+        while left < right {
+            let area = (right - left) as i32 * cmp::min(height[left], height[right]);
+            max_area = cmp::max(area, max_area);
+            if height[left] < height[right] {
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+        return max_area;
+    }
+}
